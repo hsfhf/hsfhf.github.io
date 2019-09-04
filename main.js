cb03e588270872cc7d54e185e8fc08a900451ae2
hsfhf <yhyhsfhf@outlook.com>
2019-09-04 11:47:31 +0800

    test

---
+++ main.js
@@ -0,0 +1,30 @@
+let myImage = document.querySelector('img');
+let mySrc;
+myImage.onclick = function() {
+    mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/file_code.svg') {
+      myImage.setAttribute('src', 'images/file_open.svg');
+    } else {
+      myImage.setAttribute('src', 'images/file_code.svg');
+    }
+}
+function setHeading(name) {
+    let myHeading = document.querySelector('h1');
+    myHeading.textContent = 'Mozilla 酷毙了，' + name + '！';
+  }
+  
+  function setUserName() {
+    let myName = prompt('请输入你的名字');
+    localStorage.setItem('name', myName);
+    setHeading(myName);
+  } 
+  
+  let storedName = localStorage.getItem('name');
+  if(!storedName) {
+     setUserName();
+  } else {
+     setHeading(storedName);
+  }
+  
+  let myButton = document.querySelector('button'); 
+  myButton.onclick = setUserName;
